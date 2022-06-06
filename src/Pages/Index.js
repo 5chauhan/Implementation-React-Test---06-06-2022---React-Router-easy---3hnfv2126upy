86bcd5f3a8e2eeb9c9d01440e2677bdbd6993de3
Sourabh Chauhan <isurabhkumar@gmail.com>
2022-06-06 20:21:01 +0530

    Update Index.js

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Index = () =>{
     return (
+        
+          <div id= "index-page">
+        <h1>Index Page</h1>
+        
+        </div>
      
     )
 }
