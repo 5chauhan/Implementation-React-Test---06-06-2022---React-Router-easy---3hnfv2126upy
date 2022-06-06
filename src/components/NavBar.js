3c1612eeadfdbb196fa4ffa4fd32b6cfe83b07e7
Sourabh Chauhan <isurabhkumar@gmail.com>
2022-06-06 20:32:45 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Homepage from './home-page'  
-import index from './index-page'  
+import Home from './Home'  
+import Index from './Index'  
 
 
 export const NavBar = () =>{
     return(
         <nav>
 <ul>
-         <li class="home-link" > <Route path="/home-page" component={home-page} />  </li>
-         <li class="index-link" > <Route path="/index-page" component={index-page} />  </li>
+         <li class="home-link" > <Route path="/Home" component={Home} />  </li>
+         <li class="index-link" > <Route path="/Index" component={Index} />  </li>
      
 </ul>
 </nav>
