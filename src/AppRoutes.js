80c8161f6f2368c9b34e71aa75ad206b49cc516b
Sourabh Chauhan <isurabhkumar@gmail.com>
2022-06-06 20:34:56 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export const AppRoutes = () => {
     return (
+          <Route path="/Home" component={Home} />  
+          <Route path="/Index" component={Index} />  
+              <Route path="/Notfound" component={Notfound} />  
 
     )
 }
